3075faa3eca9bc7b7874b8b9000e9886da32ccbb
facolombani <facolombani@gmail.com>
2018-02-05 19:45:59 +0100

    input de recherche + acces foursquare

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -7,5 +7,21 @@ angular.module("dolisttest")
     })
 
 function home($resource) {
-this.message = "Hello world."
+
+    this.lookUp = () => {
+        let category = "";
+        let url = "https://api.foursquare.com/v2/venues/search?near=" + this.ville + "&query=" + (this.query == undefined ? "" : this.query) + "&v=20180101";
+        let nearVenuesBdd = $resource(url, AUTH);
+        let nearVenues = nearVenuesBdd.get().$promise.then((data) => {
+            debugger;
+            let cityvenues = data.response.venues;
+            let len = cityvenues.length;
+            this.category = category;
+            this.cityvenues = cityvenues;
+
+            console.log(data)
+            console.log(this.cityvenues)
+        })
+    }
+
 }
