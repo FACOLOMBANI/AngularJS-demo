c8b02a322933f33694934a68a3446ace0e82b377
facolombani <facolombani@gmail.com>
2018-02-05 20:05:38 +0100

    fix ville input vide

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -22,24 +22,33 @@ function home($resource) {
      let showPosition = (position) => {
          lat = position.coords.latitude;
          long = position.coords.longitude;
-         getRecomm(lat, long, "")
      }
  
-     getLocation();
+    getLocation();
+    
+
 
     this.lookUp = () => {
         let category = "";
-        let url = "https://api.foursquare.com/v2/venues/search?near=" + this.ville + "&query=" + (this.query == undefined ? "" : this.query) + "&v=20180101";
-        let nearVenuesBdd = $resource(url, AUTH);
-        let nearVenues = nearVenuesBdd.get().$promise.then((data) => {
-            let cityvenues = data.response.venues;
-            let len = cityvenues.length;
-            this.category = category;
-            this.cityvenues = cityvenues;
+        if (this.ville == undefined || this.ville == "") {
+            angular.element('#ville').popover({
+                content: "Veuillez bien indiquer le nom de la ville.",
+                placement: 'top',
+            }).popover('show');
+        } else {
+            angular.element('#ville').popover('hide');
+            let url = "https://api.foursquare.com/v2/venues/search?near=" + this.ville + "&query=" + (this.query == undefined ? "" : this.query) + "&v=20180101";
+            let nearVenuesBdd = $resource(url, AUTH);
+            let nearVenues = nearVenuesBdd.get().$promise.then((data) => {
+                let cityvenues = data.response.venues;
+                let len = cityvenues.length;
+                this.category = category;
+                this.cityvenues = cityvenues;
 
-            console.log(data)
-            console.log(this.cityvenues)
-        })
-    }
+                console.log(data)
+                console.log(this.cityvenues)
+            })
+        }
+    }    
 
 }
